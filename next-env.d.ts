f1215ce67dd1d912c055987c0b35dcc18d705101
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-04-06 12:17:14 +0000

    Fix Railway deployment: Add CSS module declarations to resolve TypeScript build error

---
+++ next-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// CSS Modules
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
