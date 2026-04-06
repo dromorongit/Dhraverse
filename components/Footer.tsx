99f37c9f0b102b1b914afd49e478b2e94a606590
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-04-06 15:43:58 +0000

    Update copyright year to 2026

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -51,7 +51,7 @@ export function Footer() {
         </div>
         <div className="mt-8 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2024 Dhraverse. All rights reserved.
+            &copy; 2026 Dhraverse. All rights reserved.
           </p>
         </div>
       </div>
